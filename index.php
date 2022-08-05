a2d0ed61367ccbb182ac3132b05f5adb3add9aa2
nurdiantoro10 <nurdiantoro10@gmail.com>
2022-08-05 16:48:35 +0700

    Update 1.1

---
+++ index.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+    <link rel="stylesheet" href="assets/output.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Great+Vibes&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;1,300;1,400;1,500;1,600;1,700;1,800&display=swap" rel="stylesheet">
+</head>
+
+<body class="w-sm">
+    <div class="relative mb-4">
+        <img src="assets/tunangan.JPG" alt="">
+        <img src="assets/curve.png" alt="" class="absolute w-full -bottom-1">
+    </div>
+    <div class="text-center mb-8">
+        <h1>Nur Diantoro</h1>
+        <h1>&</h1>
+        <h1>Lidya Rahma</h1>
+    </div>
+
+    <div class="text-center mb-8 mx-4">
+        <p>Save the date</p>
+        <p class="font-bold text-blue-600 mb-4">Sabtu, 10 September 2022</p>
+        <p>Bismillahirrahmanirrahimm</p>
+        <p class="text-justify">Dengan memohon rahmat dan ridho Allah SWT, kami mengundang Bapak/Ibu/Saudara/i untuk menghadiri pernikahan Putra-Putri kami yang insya Allah akan dilaksanakan pada :</p>
+    </div>
+    <div class="text-center mb-8">
+        <h2>ACARA</h2>
+        <div class="border rounded mx-4 p-4 mb-4">
+            <h3 class="text-red-600">Akad Nikah</h3>
+            <p>Sabtu, 10 September 2022</p>
+            <p class="text-green-500">09:00 AM WIB</p>
+            <p>Jl jati petamburan 1 Gg. IV no.23</p>
+        </div>
+        <div class="border rounded mx-4 p-4">
+            <h3 class="text-red-600">Resepsi</h3>
+            <p>Sabtu, 10 September 2022</p>
+            <p class="text-green-500">02:00 PM WIB</p>
+            <p>Jl jati petamburan 1 Gg. IV</p>
+        </div>
+    </div>
+
+    <div class="">
+        <h2>LOKASI</h2>
+        <p>Petamburan, Kecamatan Tanah Abang, Kota Jakarta Pusat, Daerah Khusus Ibukota Jakarta, Indonesia</p>
+        <div class="border rounded mx-4">
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d991.6369815491014!2d106.80872382922672!3d-6.191191666436952!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xc699003e2daee4ce!2zNsKwMTEnMjguMyJTIDEwNsKwNDgnMzMuNCJF!5e0!3m2!1sid!2sid!4v1659687760599!5m2!1sid!2sid" width="100%" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+        </div>
+        <a href="https://goo.gl/maps/HURRczLgS4FazoNf9">Buka di Maps</a>
+    </div>
+
+    <div>
+        <h2>BERIKAN UCAPAN</h2>
+        <form>
+            <span>Nama</span>
+            <input type="text" name="" id="">
+            <span>Masukkan ucapanmu</span>
+            <input type="text" name="" id="">.
+            <button type="submit"></button>
+        </form>
+    </div>
+</body>
+
+</html>
