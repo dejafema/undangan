185ff7e8808fd5d633feb4625933257c03072414
nurdiantoro10 <nurdiantoro10@gmail.com>
2022-08-07 01:56:30 +0700

    Production v.1.1

---
+++ connection.php
@@ -0,0 +1,16 @@
+<?php
+// $servername = "localhost";
+// $username = "root";
+// $password = "";
+$servername = "localhost";
+$username = "u669048266_wedding";
+$password = "Password123!";
+$dbname = "u669048266_wedding";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+// echo "Connected successfully";
+$tampil = mysqli_query($conn, "SELECT * FROM ucapan ORDER BY id DESC");
--- index.php
+++ index.php
@@ -8,64 +8,123 @@
     <title>Document</title>
 
     <link rel="stylesheet" href="assets/output.css">
+    <link rel="stylesheet" href="assets/style.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Great+Vibes&display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;1,300;1,400;1,500;1,600;1,700;1,800&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Dancing+Script:wght@400;500;600;700&display=swap" rel="stylesheet">
+    <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.2/css/all.min.css" rel="stylesheet">
+
 </head>
 
-<body class="w-sm">
+<body>
+    <?php include('connection.php') ?>
+
     <div class="relative mb-4">
         <img src="assets/tunangan.JPG" alt="">
         <img src="assets/curve.png" alt="" class="absolute w-full -bottom-1">
     </div>
-    <div class="text-center mb-8">
-        <h1>Nur Diantoro</h1>
+    <div class="text-center mb-20">
+        <h1 data-aos="fade-right" data-aos-delay="100" data-aos-duration="2500">Nur Diantoro</h1>
         <h1>&</h1>
-        <h1>Lidya Rahma</h1>
+        <h1 data-aos="fade-left" data-aos-delay="100" data-aos-duration="2500">Lidya Rahma</h1>
+        <p class="mt-4">Save the date</p>
+        <p class="font-bold text-blue-600 mb-4">Sabtu, 10 September 2022</p>
     </div>
 
-    <div class="text-center mb-8 mx-4">
-        <p>Save the date</p>
-        <p class="font-bold text-blue-600 mb-4">Sabtu, 10 September 2022</p>
+    <div class="text-center mb-20 mx-4">
         <p>Bismillahirrahmanirrahimm</p>
         <p class="text-justify">Dengan memohon rahmat dan ridho Allah SWT, kami mengundang Bapak/Ibu/Saudara/i untuk menghadiri pernikahan Putra-Putri kami yang insya Allah akan dilaksanakan pada :</p>
     </div>
-    <div class="text-center mb-8">
-        <h2>ACARA</h2>
-        <div class="border rounded mx-4 p-4 mb-4">
-            <h3 class="text-red-600">Akad Nikah</h3>
+
+    <div class="text-center mb-20">
+        <h2>Acara</h2>
+        <div class="border rounded mx-4 py-12 mb-4 shadow-md relative overflow-hidden" data-aos="fade-right">
+            <img src="assets/bunga-03.png" class="h-40 absolute top-1/2 -translate-y-1/2 -left-20">
+            <h3 class=" text-red-600">Akad Nikah</h3>
             <p>Sabtu, 10 September 2022</p>
-            <p class="text-green-500">09:00 AM WIB</p>
+            <p class="text-emerald-600 font-bold">10:00 WIB</p>
             <p>Jl jati petamburan 1 Gg. IV no.23</p>
         </div>
-        <div class="border rounded mx-4 p-4">
+        <div class="border rounded mx-4 py-12 shadow-md relative overflow-hidden" data-aos="fade-left">
+            <img src="assets/bunga-05.png" class="h-40 absolute top-1/2 -translate-y-1/2 -right-16">
             <h3 class="text-red-600">Resepsi</h3>
             <p>Sabtu, 10 September 2022</p>
-            <p class="text-green-500">02:00 PM WIB</p>
+            <p class="text-emerald-600 font-bold">12:00 - 15:00 WIB</p>
             <p>Jl jati petamburan 1 Gg. IV</p>
         </div>
     </div>
 
-    <div class="">
-        <h2>LOKASI</h2>
-        <p>Petamburan, Kecamatan Tanah Abang, Kota Jakarta Pusat, Daerah Khusus Ibukota Jakarta, Indonesia</p>
-        <div class="border rounded mx-4">
-            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d991.6369815491014!2d106.80872382922672!3d-6.191191666436952!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xc699003e2daee4ce!2zNsKwMTEnMjguMyJTIDEwNsKwNDgnMzMuNCJF!5e0!3m2!1sid!2sid!4v1659687760599!5m2!1sid!2sid" width="100%" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+    <div class="text-center mx-4 mb-20">
+        <h2>Lokasi</h2>
+        <p class="text-sm">Jl Jati Petamburan I, Kecamatan Tanah Abang, Kota Jakarta Pusat, Daerah Khusus Ibukota Jakarta, Indonesia</p>
+        <div class="border rounded-lg overflow-hidden shadow-md">
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d991.6369815491014!2d106.80872382922672!3d-6.191191666436952!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xc699003e2daee4ce!2zNsKwMTEnMjguMyJTIDEwNsKwNDgnMzMuNCJF!5e0!3m2!1sid!2sid!4v1659687760599!5m2!1sid!2sid" width="100%" height="200" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
         </div>
-        <a href="https://goo.gl/maps/HURRczLgS4FazoNf9">Buka di Maps</a>
+        <a href="https://goo.gl/maps/HURRczLgS4FazoNf9" class="w-full bg-gradient-to-r from-cyan-500 to-blue-500 rounded shadow px-4 py-2 text-white block mt-4">Buka di Maps</a>
     </div>
 
-    <div>
-        <h2>BERIKAN UCAPAN</h2>
-        <form>
-            <span>Nama</span>
-            <input type="text" name="" id="">
-            <span>Masukkan ucapanmu</span>
-            <input type="text" name="" id="">.
-            <button type="submit"></button>
+    <div class="mx-4 mb-20">
+        <img src="assets/bunga.png" alt="" class="w-full">
+    </div>
+
+    <div class="mx-4 mb-20">
+        <h2 class="text-center">Amplop Digital</h2>
+        <p class="text-center text-sm">Do'a restu anda merupakan karunia yang sangat berarti bagi kami. Jika ada yang ingin memberikan amplop digital untuk pernikahan kami, kami menyediakan pilihan Cashless di bawah ini</p>
+        <div class="bg-white rounded shadow-md p-4 mb-2" data-aos="fade-right" data-aos-duration="1000">
+            <p class="text-sm mb-2">Nur Diantoro</p>
+            <div class="flex">
+                <img src="assets/BCA.svg" width="60px">
+                <input type="text" value="1110159737" id="nur" class="ml-4">
+                <button class="ml-auto" onclick="copyToClipBoard('nur')">Salin</=>
+            </div>
+        </div>
+        <div class="bg-white rounded shadow-md p-4" data-aos="fade-left" data-aos-duration="1000">
+            <p class="text-sm mb-2">Lidya Rahma</p>
+            <div class="flex">
+                <img src="assets/BCA.svg" width="60px">
+                <input type="text" value="1110153933" id="lidya" class="ml-4">
+                <span class="ml-auto" onclick="copyToClipBoard('lidya')">Salin</span>
+            </div>
+        </div>
+    </div>
+
+    <div class="mb-20 mx-4">
+        <h2 class="text-center">Berikan Ucapan</h2>
+        <form method="post" action="send-ucapan.php">
+            <div class="w-full relative mb-4">
+                <input type="text" name="nama" id="username" class="px-4 py-2 w-full border-sky-400 focus-visible:outline-none focus:shadow-md border rounded text-input" required>
+                <label for="username" class="absolute left-4 top-1/2 -translate-y-1/2 label duration-200">Nama</label>
+            </div>
+            <div class="w-full relative mb-4">
+                <textarea type="text" name="pesan" id="username" class="px-4 py-2 w-full border-sky-400 focus-visible:outline-none focus:shadow-md border rounded text-input" required rows="3"></textarea>
+                <label for="username" class="absolute left-4 top-4 -translate-y-1/2 label duration-200">Pesan</label>
+            </div>
+            <button type="submit" name="submit" class="px-12 py-2 rounded bg-sky-600 text-white mx-auto block">Kirim</button>
         </form>
     </div>
+
+    <div class="bg-slate-200 py-10">
+        <div class="mb-20 mx-4">
+            <h2 class="text-center">Ucapan Mereka</h2>
+            <?php while ($ucapan = mysqli_fetch_array($tampil)) { ?>
+                <div class="rounded shadow-md bg-white p-4 relative overflow-hidden mb-4" data-aos="zoom-in" data-aos-anchor-placement="center-bottom">
+                    <p class="text-red-600 font-bold"><?= $ucapan['nama']  ?></p>
+                    <p class="italic text-sm"><?= $ucapan['pesan'] ?></p>
+                </div>
+            <?php } ?>
+        </div>
+    </div>
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+    <script src="assets/main.js"></script>
+
+    <script>
+        AOS.init();
+    </script>
 </body>
 
 </html>
